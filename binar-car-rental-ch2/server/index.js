d662dd985355639ed44b6ed7a09032e189f42d74
raflikharismaa <raflikharisma29@webmail.umm.ac.id>
2024-03-15 00:57:26 +0700

    fix: dom filter bugs

--- binar-car-rental-ch2/server/index.js
+++ binar-car-rental-ch2/server/index.js
@@ -1,21 +1,41 @@
-//core package
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-
 const url = require("url");
-
-const PUBLIC_DIR = path.join(__dirname, "../public");
+const PUBLIC_DIRECTORY = path.join(__dirname, "../public");
 const PORT = 8000;
 
 const server = (req, res) => {
   if (req.url === "/") {
-    res.end("INI DEFAULT SERVER");
-  } else if (req.url === "/search") {
-    res.end("INI HALAMAN SEARCH");
+    req.url = "/index.html";
+  } else if (req.url === "/cars") {
+    req.url = "/cars.html";
   } else {
-    res.end("empty");
+    req.url = req.url;
   }
+  const parseURL = url.parse(req.url);
+  const pathName = `${parseURL.pathname}`;
+  const extension = path.parse(pathName).ext;
+  const absolutePath = path.join(PUBLIC_DIRECTORY, pathName);
+
+  const contentTypes = {
+    ".css": "text/css",
+    ".png": "image/png",
+    ".svg": "image/svg+xml",
+    ".html": "text/html",
+    ".js": "text/javascript",
+  };
+
+  fs.readFile(absolutePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("File not found ...");
+    } else {
+      res.setHeader("Content-Type", contentTypes[extension] || "text/plain");
+      res.end(data);
+    }
+  });
 };
+
 http.createServer(server).listen(PORT);
-console.log(`Server is Running..PORT: ${PORT}`);
+console.log(`Server is running ... PORT : localhost:${PORT}`);
